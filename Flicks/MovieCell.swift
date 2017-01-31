d1bb14b04e5ff5e4bbf8dec5a255992ca6615536
Kausthub Poondi <kpoondi@Kausthubs-MacBook-Pro.local>
2017-01-30 23:55:55 -0600

    prework done

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Kausthub Poondi on 1/30/17.
+//  Copyright © 2017 Kausthub Poondi. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var overview: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
